21001d87dfc09599379837140ae37d5a6b0e4740
Manny Delgado <mannygdp@gmail.com>
2023-08-28 21:18:45 -0700

    Built ReactDom Site

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,10 +1,12 @@
 
-import './App.css';
 
 import Home from './components/Home'
 import About from './components/About'
 import Packages from './components/Packages'
+import { BrowserRouter as Router, Link, Routes, Route }
+from 'react-router-dom'
 
+import './App.css';
 
 function App() {
 
@@ -12,31 +14,35 @@ function App() {
 
   return (
     <div className="App">
-      <header>
+      <Router>
+        <header>
         <h1 className="title">Welcome to Monty's Mineral SPA</h1>
-
         <div className="navBar">
           <ul>
             <li>
-              <a href={<Home />}>Home</a>
+              <Link to="/">Home</Link>
             </li>
             <li>
-              <a href={<About />}>About Us</a>
+            <Link to="/about">About</Link>
             </li>
             <li>
-              <a href={<Packages packages={packages}/>}>Our Packages</a>
+            <Link to="/packages">Packages</Link>
             </li>
           </ul>
         </div>
-
       </header>
+
+      <div className="display">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/packages" element={<Packages packages={packages}/>} />
+        </Routes>
+      </div>
+      
+  </Router>
     </div>
   );
 }
 
 export default App;
-
-
-
-
-
